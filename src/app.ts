60b935b748145709c736d7c5868c6f89f13c00c5
WendelLorenzi <wlorenzi17@gmail.cm>
2025-05-05 20:19:18 -0300

    feat: update API documentation and remove unused Thunder Client collection

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ require('dotenv').config();
 import express from 'express'
 import { router } from './routes'
 import cors from 'cors';
-// import swagguerUi from 'swagger-ui-express';
-// import swaggerDocs from './swagger.json';
 import bodyParser from 'body-parser';
 import connectDB from './providers/mongoDB/connection';
 
@@ -17,8 +15,8 @@ app.use(express.urlencoded());
 app.use(bodyParser.json());                        
 app.use(bodyParser.urlencoded({ extended: true }));
 connectDB();
-//app.use('/docs', swagguerUi.serve as any, swagguerUi.setup(swaggerDocs) as any);
 app.use('/v1', router);
+
 
 export { app };
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import { userRouter } from './user';
 import { uTokenRouter } from './userToken';
-
+import swagguerUi from 'swagger-ui-express';
+import swaggerDocs from '../doc/swagger.json';
 
 export const router = Router();
 
 router.get('/health', (req, res) => {
     res.status(200).json({ message: 'Server is running' });
 });
+router.use('/docs', swagguerUi.serve as any, swagguerUi.setup(swaggerDocs) as any);
 router.use(userRouter);
 router.use(uTokenRouter);
 
